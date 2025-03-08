ec4517e37d9a766587dbe6eb26b84960004431cf
itsvikash93 <vikashkumar827070@gmail.com>
2025-03-08 17:01:20 +0530

    api changed

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
   // baseURL: "https://apollo-expeditions.onrender.com/api",
-  // baseURL: "https://apollo-expeditions-kdga.onrender.com/api",
+  baseURL: "https://apollo-expeditions-kdga.onrender.com/api",
 });
 
 export default instance;
